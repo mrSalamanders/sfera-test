86a9cf0d728b70d8e9a9a733c5d7ea1f0397fdf4
mrSalamanders <nikvlad2000@gmail.com>
2021-08-28 18:43:16 +0300

    Настроена иерархия контроллеров

--- sfera-test/SceneDelegate.swift
+++ sfera-test/SceneDelegate.swift
@@ -18,10 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()    // ViewController.swift
-        window?.rootViewController?.view.backgroundColor = .yellow
+        window?.rootViewController = UINavigationController.init(rootViewController: TableViewController())
         window?.makeKeyAndVisible()
     }
 
